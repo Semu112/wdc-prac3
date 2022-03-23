c0007764a58264d241e40bdcf2d648b0e3b3cbaf
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-03-23 03:43:01 +0000

    Wed, Mar 23, 2022, 3:43 AM +00:00

--- task1.js
+++ task1.js
@@ -1,4 +1,6 @@
-function showTime(){
+function showTime() {
     date = new Date();
     document.getElementById('current_time').innerText = date.toLocaleString();
 };
+hit ther e
+ths aoaiooi::; joie ()()()()()()
