5d7bedcb1093778c49f088feb787d5485a05405d
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-03-23 06:33:00 +0000

    Wed, Mar 23, 2022, 6:33 AM +00:00

--- task3.js
+++ task3.js
@@ -2,5 +2,5 @@ let counter = 0;
 
 function incrementCounter() {
     counter++;
-    document.getElementById('mcount').innerText = counter
+    document.getElementById('mcount').innerText = counter;
 }
