c577517b001f349f590c55e61558ee2ba68914df
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-03-23 03:15:05 +0000

    Wed, Mar 23, 2022, 3:15 AM +00:00

--- task1.js
+++ task1.js
@@ -1,4 +1,6 @@
 function showTime(){
     date = new Date();
-    document.getElementById('current_time').innerText = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " date.getHours() + ":" date.getMinutes();
+    date = date.toLocaleString();
+    //document.getElementById('current_time').innerText = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear() + " " date.getHours() + ":" date.getMinutes();
+    //document.getElementById('current_time').innerText = date;
 };
