c73083a43a936934b3e2e98149b287117fa9d09d
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-03-24 07:11:58 +0000

    #10

--- task3.js
+++ task3.js
@@ -4,3 +4,12 @@ function incrementCounter() {
     counter++;
     document.getElementById('mcount').innerText = counter;
 }
+
+function post() {
+    var postText = document.getElementById('textArea').value;
+
+    var date = new Date();
+    document.getElementsByClassName('post-time')[0].innerText = date.toLocaleString();
+
+    document.getElementsByClassName('post-content')[0].innerText = postText;
+}
