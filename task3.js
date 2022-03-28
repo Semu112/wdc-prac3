8383341f007a83c67ac4071ce6ac7bc060de18f8
Semu112 <83565458+Semu112@users.noreply.github.com>
2022-03-28 01:26:59 +0000

    #15

--- task3.js
+++ task3.js
@@ -1,3 +1,17 @@
+function hideSomePosts(){
+
+    var amtOfPostsToShow = document.getElementById("slider").value;
+
+    for(let i = 0; i<amtOfPostsToShow; i++){
+        document.getElementsByClassName("post-time")[i].style.display = "block";
+        document.getElementsByClassName("post-content")[i].style.display = "block";
+    }
+    for(let i = amtOfPostsToShow; i<10; i++){
+        document.getElementsByClassName("post-time")[i].style.display = "none";
+        document.getElementsByClassName("post-content")[i].style.display = "none";
+    }
+}
+
 function changeBackgroundColour(){
     document.body.style.background = document.getElementById("backgroundColor").value;
 }
@@ -31,13 +45,23 @@ function post() {
         }
     }
 
+    var fontWeight;
+    var fontStyle;
+
+    //Post weight and italics
+    if(document.getElementById("bold").checked == true){
+        fontWeight = "bold";
+    }
+    if(document.getElementById("italics").checked == true){
+        fontStyle = "italic";
+    }
+
     var date = new Date();
     date = date.toLocaleString();
 
     for(let i = 0; i<document.getElementById("quantity").value; i++){
 
         //Post time
-
         var newPostTime = document.createElement('DIV');
         newPostTime.classList.add("post-time");
         newPostTime.innerText = date;
@@ -47,26 +71,10 @@ function post() {
         newPostContent.classList.add("post-content");
         newPostContent.innerText = postText;
         newPostContent.style.color = postColor;
+        newPostContent.style.fontWeight = fontWeight;
+        newPostContent.style.fontStyle = fontStyle;
 
         document.getElementById("posts").appendChild(newPostTime);
         document.getElementById("posts").appendChild(newPostContent);
-        /*
-
-        const timeNode = document.querySelector("#posts > .post-time").lastChild;
-        const timeNodeAttributes = document.querySelector("#posts > .post-time").attributes;
-
-        const contentNode = document.querySelector("#posts > .post-content").lastChild;
-        const contentNodeAttributes = document.querySelector("#posts > .post-content").attributes;
-
-
-        const clonedTime = timeNode.cloneNode(true);
-        clonedTime.attributes = timeNodeAttributes;
-
-        const clonedContent = contentNode.cloneNode(true);
-        clonedContent.attributes = contentNodeAttributes;
-
-        document.getElementById("posts").appendChild(clonedTime);
-        document.getElementById("posts").appendChild(clonedContent);
-        */
     }
 }
